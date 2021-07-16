4be559f599255829dfc087f7dac914e1a827e1cc
gonese <cch61310@gmail.com>
2021-07-16 12:26:40 -0400

    Add login page

---
+++ my-app/src/components/Sub/login.vue
@@ -0,0 +1,51 @@
+<template>
+    
+    <div>
+        <div>
+            <el-input placeholder="enter username" v-model="name" clearable class = ''></el-input>
+            <span v-if="error.name" class="err-msg">{{error.name}}</span>
+        </div>
+        <div>
+            <el-input placeholder="enter password" v-model="password" show-password class="input-style"> </el-input>
+            <span v-if="error.pwd" class="err-msg">{{error.pwd}}</span>
+        </div>
+        <div>
+            <el-button type="primary" @click="login" class="login_style">Log In</el-button>
+        </div>
+    </div>
+</template>
+<script>
+    export default{
+        name:"Login",
+        data(){
+            return {
+                name:"",
+                password:"",
+                error:{
+                    name:"",
+                    password:""
+                }
+            }
+
+        },
+        methods:{
+            login(){
+                const {name,password,$router} = this
+                this.$router.push({
+                    name:"Home",
+                    parameters:{
+                        username: this.name
+                    }
+                })
+
+            },
+            debug (event) {
+                console.log(event.target.name)
+            }
+        }
+
+
+
+    }
+
+</script>
--- my-app/src/main.js
+++ my-app/src/main.js
@@ -3,12 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import BootstrapVue from 'bootstrap-vue'
+import ElementUI from 'element-ui'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
