50173ee92bb8b738dcff02bdef0b745d0e043490
gonese <cch61310@gmail.com>
2021-07-17 21:49:27 -0400

    register

---
+++ my-app/src/components/Sub/register.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <form id="register">
+            <div>
+                <p>User Name:</p>
+                <input v-model="username" placeholder="username">
+            </div>
+            <div>
+                <p>password:</p>
+                <input v-model="password" placeholder="password">
+            </div>
+            <div>
+                <p>email:</p>
+                <input v-model="Email" placeholder="password">
+            </div>
+            <div>
+                <p>phone number:</p>
+                <input v-model="Phone" placeholder="password">
+            </div>
+        </form>
+    </div>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
